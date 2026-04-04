64c9841181f8a1e944f2019303d0f601329096bb
Amar Vankayalapati <amar.v0225@gmail.com>
2026-04-04 09:44:00 +0000

    Cleanup: Remove ignored files from tracking

--- .venv/lib/python3.12/site-packages/arcadepy/types/shared/__init__.py
+++
@@ -1,5 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from .error import Error as Error
-from .authorization_context import AuthorizationContext as AuthorizationContext
-from .authorization_response import AuthorizationResponse as AuthorizationResponse
